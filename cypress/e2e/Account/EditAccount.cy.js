9b7f147d94512a49807160c22de94b8737d4401d
BoatGT-9 <twz192545@gmail.com>
2024-07-11 19:39:51 +0700

    update element

--- cypress/e2e/Account/EditAccount.cy.js
+++ cypress/e2e/Account/EditAccount.cy.js
@@ -1,48 +1,48 @@
 const BASE_URL = "http://localhost:5173";
 describe("Edit account", () => {
   // เปลี่ยนชื่อ สำเร็จ
-  it("edit account firstName and lastName", () => {
-    cy.visit(BASE_URL);
-    // login account
-    cy.contains("Sign In").click();
-    cy.get("div:nth-child(1) > .relative > #username").type("boat");
-    cy.get("div:nth-child(1) > .relative > #password").type("19092545Boat");
-    cy.get("#setup-user-drawer-submit").click();
-    // edit Name
-    cy.get("#user-profile-image").click();
-    cy.get("#edit-profile-drawer-nav-link").click();
-    cy.get("#firstName").click();
-    cy.get("#firstName").clear();
-    cy.get("#firstName").type(`User_firstName`);
-    cy.get("#lastName").click();
-    cy.get("#lastName").clear();
-    cy.get("#lastName").type("User_lastName");
-    // รอใส่ตัวสมบูรณ์
-    cy.get("#save-update-new-user-info-btn").click();
-    cy.screenshot();
-  });
+  // it("edit account firstName and lastName", () => {
+  //   cy.visit(BASE_URL);
+  //   // login account
+  //   cy.contains("Sign In").click();
+  //   cy.get("div:nth-child(1) > .relative > #username").type("boat");
+  //   cy.get("div:nth-child(1) > .relative > #password").type("19092545Boat");
+  //   cy.get("#setup-user-drawer-submit").click();
+  //   // edit Name
+  //   cy.get("#user-profile-image").click();
+  //   cy.get("#edit-profile-drawer-nav-link").click();
+  //   cy.get("#firstName").click();
+  //   cy.get("#firstName").clear();
+  //   cy.get("#firstName").type(`User_firstName`);
+  //   cy.get("#lastName").click();
+  //   cy.get("#lastName").clear();
+  //   cy.get("#lastName").type("User_lastName");
+  //   // รอใส่ตัวสมบูรณ์
+  //   cy.get("#save-update-new-user-info-btn").click();
+  //   cy.screenshot();
+  // });
 
   // // ชื่อไม่แมท
-  it("Edit firstName not match pattern", () => {
-    cy.visit(BASE_URL);
-    // login account
-    cy.contains("Sign In").click();
-    cy.get("div:nth-child(1) > .relative > #username").type("boat");
-    cy.get("div:nth-child(1) > .relative > #password").type("19092545Boat");
-    cy.get("#setup-user-drawer-submit").click();
-    // edit Name
-    cy.get("#user-profile-image").click();
-    cy.get("#edit-profile-drawer-nav-link").click();
-    cy.get("#firstName").click();
-    cy.get("#firstName").clear();
-    cy.get("#firstName").type(`User_firstName${new Date().getSeconds()}`);
-    cy.get("#lastName").click();
-    cy.get("#lastName").clear();
-    cy.get("#lastName").type("User_lastName");
-    // รอใส่ตัวสมบูรณ์
-    cy.get("#save-update-new-user-info-btn").click();
-    cy.screenshot();
-  });
+  // it("Edit firstName not match pattern", () => {
+  //   cy.visit(BASE_URL);
+  //   // login account
+  //   cy.contains("Sign In").click();
+  //   cy.get("div:nth-child(1) > .relative > #username").type("boat");
+  //   cy.get("div:nth-child(1) > .relative > #password").type("19092545Boat");
+  //   cy.get("#setup-user-drawer-submit").click();
+  //   // edit Name
+  //   cy.get("#user-profile-image").click();
+  //   cy.get("#edit-profile-drawer-nav-link").click();
+  //   cy.get("#firstName").click();
+  //   cy.get("#firstName").clear();
+  //   cy.get("#firstName").type(`User_firstName${new Date().getSeconds()}`);
+  //   cy.get("#lastName").click();
+  //   cy.get("#lastName").clear();
+  //   cy.get("#lastName").type("User_lastName");
+  //   // รอใส่ตัวสมบูรณ์
+  //   cy.get("#save-update-new-user-info-btn").click();
+  //   cy.screenshot();
+  // });
 
   //   if (
   //     ("file Upload",
@@ -67,8 +67,10 @@ describe("Edit account", () => {
     cy.get("#user-profile-image").click();
     cy.get("#edit-profile-drawer-nav-link").click();
 
-    cy.contains("#update-user-profile-btn").click();
+    cy.get("#update-user-profile-btn").click();
+    cy.wait(2000)
     cy.get("#file-drop-zone").click();
+    cy.wait(5000)
   });
   // //   ลบรูปภาพโปรไฟล์
   // it("Delete Photo Profile", () => {
