4ffffe9dfad4081143ffb77b4b9b9cc51bfefaba
aumnatthacha <aummumdaeng@gmail.com>
2024-07-12 21:08:46 +0700

    update_add Device list

--- cypress/e2e/device/test_add_devicelist.cy.js
+++ cypress/e2e/device/test_add_devicelist.cy.js
@@ -2,41 +2,40 @@ import { login } from "./spec.cy";
 
 describe("Device List Test", () => {
   it("should login successfully with correct credentials and perform device operations", () => {
+    // Login
     login("AumNatthacha", "Natthacha2002");
 
     // Add Device
     cy.get("#devices-nav-link-sidebar").click();
-    /*
+    cy.contains("Devices").click();
     cy.get("#setup-user-submit").click();
-    cy.get("#nameDevice").type("อุปกรณ์การเกษตร", { force: true });
-    cy.get("#usernameDevice").type("K-001", { force: true });
-    cy.get("#password").type("K001-K", { force: true });
-    cy.get("#description").type("อุปกรณ์การเกษตร", { force: true });
-    cy.get("#topics").type("K-001", { force: true });
-    // cy.get("#Qos").type("1", { force: true });
+    cy.get("#nameDevice").type("Sensor_01");
+    cy.get("#usernameDevice").type("user");
+    cy.get("#password").type("password123");
+    cy.get("#description").type("แดชบอร์ดนี้ใช้ในการตรวจสอบข้อมูลจาก Sensor_01 เช่น อุณหภูมิและความชื้น");
+    cy.get("#topics").type("ข้อมูลส่งทุก 10 นาที");
+    cy.get("#Qos").type("0");
     cy.get("#retain-checkbox").click();
     cy.get("#add-device-submit-btn").click();
-    */
+    cy.contains("Created your Sensor_01 device").should("be.visible");
+    cy.screenshot();
 
     // Edit Device
-    /*
-    cy.get("#6683ab5fc7b871479e612507-edit-device-btn").click();
-    cy.get("#nameDevice").clear().type("อุปกรณ์การเกษตร 2", { force: true });
-    cy.get("#usernameDevice").clear().type("K-0012", { force: true });
-    cy.get("#password").clear().type("K001-K2", { force: true });
-    cy.get("#description").clear().type("อุปกรณ์การเกษตร 2", { force: true });
-    // cy.get("#Qos").type("", { force: true });
-    cy.get("#topics").clear().type("K-0012", { force: true });
+    cy.get("#669135ccfed9b7834140e10f-edit-device-btn").click();
+    cy.get("#nameDevice").clear().type("Sensor_02");
+    cy.get("#usernameDevice").clear().type("user");
+    cy.get("#password").clear().type("password123");
+    cy.get("#description").clear().type("อุณหภูมิและความชื้น");
+    cy.get("#topics").clear().type("ข้อมูลส่งทุก 11 นาที");
     cy.get("#submit-edit-dashboard-btn").click();
-    */
+    cy.contains("Your device information has been edited successfully").should("be.visible");
+    cy.screenshot();
 
-    // Optionally, delete device (uncomment if needed)
-    /*
-    cy.get("#6683ab5fc7b871479e612507-delete-device-btn").click();
+    // Delete Device (Optional: Uncomment if needed)
+    cy.get("#66913723fed9b7834140e190-delete-device-btn").click();
     cy.wait(2000);
     cy.get("#confirm-delete-device").click();
-    */
-
+    cy.contains("Your device has been deleted").should("be.visible");
     cy.screenshot();
   });
 });
