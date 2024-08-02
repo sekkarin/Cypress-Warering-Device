53546d772a096105be88bd9ca541dd45da44510f
BoatGT-9 <twz192545@gmail.com>
2024-08-02 13:20:48 +0700

    device

--- cypress/e2e/device/add_device.cy.js
+++ cypress/e2e/device/add_device.cy.js
@@ -1,26 +1,39 @@
-const BASE_URL= 'http://localhost:5173'
-describe('Add Device', ()=>{
-        // login to web 
-        it("login to webpage success fully", () => {
-            cy.visit(BASE_URL);
-            cy.get("#toggle-big-login-landing-drawer-btn").click();
-            cy.get("div:nth-child(1) > .relative > #username").type("boat");
-            cy.get("div:nth-child(1) > .relative > #password").type("19092545Boat");
-            cy.get("#remember-user-drawer-checkbox").click();
-            cy.get("#setup-user-drawer-submit").click();
-            // cy.get("h1").should("contain", "Overview");
-            // cy.screenshot();
-            
-            cy.get("#devices-nav-link-sidebar").click("");
-             cy.get("#setup-user-submit").click("");
-            //  cy.get("#nameDevice").type("");
-            //  cy.get("#usernameDevice").type("");
-            //  cy.get("#password").type("");
-            //  cy.get("description").type("");
+import { login } from "./spec.cy";
 
-            //  cy.get("#topics").type("");
-            //  cy.get("#Qos").type("");
-            //  cy.get("#retain-checkbox").click()
-          });
-        
-})
+describe("Add Device", () => {
+  // login to web
+  it("TC User can Add Device ", () => {
+    login("boat", "19092545Boat");
+
+    cy.get("#devices-nav-link-sidebar").click();
+    cy.get("#setup-user-submit").click();
+     cy.get("#nameDevice").type("DV1");
+     cy.get("#usernameDevice").type("DV-1");
+     cy.get("#password").type("password123456");
+     cy.get("#description").type("Device description");
+
+     cy.get("#topics").type(`Topics Device${new Date().getSeconds()}`);
+    //  cy.get("#Qos").click();
+     cy.get("#retain-checkbox").click();
+     cy.get("#add-device-submit-btn").click();
+     cy.contains("Created your DV1 device").should("exist");
+     cy.screenshot("");
+  });
+  it("TC User can't add device NameDevice To use",()=>{
+    login("boat", "19092545Boat");
+
+    cy.get("#devices-nav-link-sidebar").click();
+    cy.get("#setup-user-submit").click();
+     cy.get("#nameDevice").type("DV1");
+     cy.get("#usernameDevice").type("DV-2");
+     cy.get("#password").type("password123456");
+     cy.get("#description").type("Device description");
+
+     cy.get("#topics").type(`Topics Device${new Date().getSeconds()}`);
+    //  cy.get("#Qos").click();
+     cy.get("#retain-checkbox").click();
+     cy.get("#add-device-submit-btn").click();
+     cy.contains("Created your DV1 device").should("exist");
+     cy.screenshot("");
+  })
+});
