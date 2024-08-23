187fa6024ec4cfcaafc25ffafdd8fa48a0fd1185
BoatGT-9 <twz192545@gmail.com>
2024-08-23 16:35:37 +0700

    update device fix update &delete

--- cypress/e2e/device/device.cy.js
+++ cypress/e2e/device/device.cy.js
@@ -1,7 +1,6 @@
 import { login } from "./spec.cy";
 
 describe("Device", () => {
-  
   it("TC4001 User can Add Device ", () => {
     login("boat"  , "19092545Boat");
 
@@ -13,15 +12,45 @@ describe("Device", () => {
     cy.get("#description").type(
       "แดชบอร์ดนี้ใช้ในการตรวจสอบข้อมูลจาก Sensor_01 เช่น อุณหภูมิและความชื้น"
     );
-    cy.get("#topics").type(` ข้อมูลส่งทุก ${new Date().getSeconds()} นาที`);
+    cy.get("#topics").type(` ข้อมูลส่งทุก ${new Date().getMilliseconds()} นาที`);
     cy.get("#Qos").type("0");
     cy.get("#retain-checkbox").click();
     cy.get("#add-device-submit-btn").click();
     cy.contains("Created your Sensor_01 device").should("be.visible");
+    cy.screenshot();
+  });
+  
+  //  Edit Device
+  it("TC4002 user can edit Device  ", () => {
+    login("boat", "19092545Boat");
+    cy.get("#devices-nav-link-sidebar").click();
+    cy.contains("Edit").click();
+    cy.get("#nameDevice").clear().type("Sensor_08");
+    cy.get("#usernameDevice").clear().type(`user kiki${new Date().getFullYear()}`);
+    cy.get("#password").clear().type("password123");
+    cy.get("#description").clear().type("อุณหภูมิและความชื้น");
+    cy.get("#topics").clear().type(`ข้อมูลส่งทุก ${new Date().getFullYear()} นาที`);
+    cy.get("#submit-edit-dashboard-btn").click();
+    cy.contains("Your device information has been edited successfully").should(
+      "exist"
+    );
+    cy.screenshot();
   });
 
+  // deleted
+  it("TC4003 User can delete device",()=>{
+    login("boat", "19092545Boat");
+    cy.get("#devices-nav-link-sidebar").click();
+
+    cy.contains("Delete").click();
+    cy.wait(2000);
+    cy.get("#confirm-delete-device").click();
+    cy.contains("Your device has been deleted").should("be.visible");
+    cy.screenshot();
+  })
+
   // username Device ซ้ำ
-  it("TC4002 UsernameDevice to used ", () => {
+  it("TC4004 UsernameDevice to used ", () => {
     login("boat", "19092545Boat");
     cy.get("#devices-nav-link-sidebar").click();
     cy.get("#setup-user-submit").click();
@@ -42,8 +71,8 @@ describe("Device", () => {
     cy.screenshot("");
   });
 
-  // ผู้ใช้ได้ใช้หัวข้อที่กำหนดให้กับอุปกรณ์อื่นแล้ว
-  it("TC4003 The user has already used the topic assigned to another device.", () => {
+  // // ผู้ใช้ได้ใช้หัวข้อที่กำหนดให้กับอุปกรณ์อื่นแล้ว
+  it("TC4005 The user has already used the topic assigned to another device.", () => {
     login("boat", "19092545Boat");
     cy.get("#devices-nav-link-sidebar").click();
     cy.get("#setup-user-submit").click();
@@ -61,8 +90,8 @@ describe("Device", () => {
     cy.contains("Topics already assigned to another device").should("exist");
     cy.screenshot("");
   });
-  // ชื่อดีไวซ์ต้องสั้นกว่าหรือเท่ากับ 25 อักขระ
-  it("TC4004 nameDevice must be shorter than or equal to 25 characters ", () => {
+  // // ชื่อดีไวซ์ต้องสั้นกว่าหรือเท่ากับ 25 อักขระ
+  it("TC4006 nameDevice must be shorter than or equal to 25 characters ", () => {
     login("boat", "19092545Boat");
     cy.get("#devices-nav-link-sidebar").click();
     cy.get("#setup-user-submit").click();
@@ -82,35 +111,6 @@ describe("Device", () => {
     cy.screenshot("");
   });
 
-  //  Edit Device
-
-  it("TC4005 user can edit Device  ", () => {
-    login("boat", "19092545Boat");
-    cy.get("#devices-nav-link-sidebar").click();
-    cy.get("#66ac9de8303ebf8fcf99f02a-edit-device-btn").click();
-    cy.get("#nameDevice").clear().type("Sensor_08");
-    cy.get("#usernameDevice").clear().type("user SumSum");
-    cy.get("#password").clear().type("password123");
-    cy.get("#description").clear().type("อุณหภูมิและความชื้น");
-    cy.get("#topics").clear().type("ข้อมูลส่งทุก 11 นาที");
-    cy.get("#submit-edit-dashboard-btn").click();
-    cy.contains("Your device information has been edited successfully").should(
-      "exist"
-    );
-    cy.screenshot();
-  });
-
-  // deleted
-  it("TC4006 User can delete device",()=>{
-    login("boat", "19092545Boat");
-    cy.get("#devices-nav-link-sidebar").click();
-
-    cy.get("#66ac9ee6303ebf8fcf99f0e6-delete-device-btn").click();
-    cy.wait(2000);
-    cy.get("#confirm-delete-device").click();
-    cy.contains("Your device has been deleted").should("be.visible");
-    cy.screenshot();
-  })
 
   it("TC4007 User not have Name Device",()=>{
     login("boat", "19092545Boat");
@@ -195,7 +195,7 @@ describe("Device", () => {
     cy.get("#setup-user-submit").click();
     cy.get("#nameDevice").type("Sensor_08");
     cy.get("#usernameDevice").type(`User${new Date().getSeconds()}`);
-    cy.get("#password").type("password123");
+    cy.get("#password").type("123");
     cy.get("#description").type(
       "แดชบอร์ดนี้ใช้ในการตรวจสอบข้อมูลจาก Sensor_01 เช่น อุณหภูมิและความชื้น"
     );
@@ -208,4 +208,53 @@ describe("Device", () => {
     );
     cy.screenshot();
   });
+
+  // search Device
+  it("TC40012 User can Search Device", () => {
+    login("boat", "19092545Boat");
+    cy.get("#devices-nav-link-sidebar").click();
+
+    cy.get("#search_device").type("Sensor_02");
+    cy.contains("Sensor_02").should("exist")
+
+    cy.screenshot();
+  });
+
+  // filter
+  it("TC40013 User can filter Sort by Date Oldest", () => {
+    login("boat", "19092545Boat");
+    cy.get("#devices-nav-link-sidebar").click();
+
+    cy.get("#sort-by-date-select").select("Oldest");
+    cy.get("#registration-66c83b7fddd4126f8900bfe0").siblings();
+    cy.screenshot();
+  });
+
+  it("TC40014 User can filter Sort by Date Latest", () => {
+    login("boat", "19092545Boat");
+    cy.get("#devices-nav-link-sidebar").click();
+
+    cy.get("#sort-by-date-select").select("Latest");
+    cy.get("#registration-66c846a2ddd4126f8900c24b").siblings();
+    cy.screenshot();
+  });
+
+  it("TC40015 User can filter permission allow", () => {
+    login("boat", "19092545Boat");
+    cy.get("#devices-nav-link-sidebar").click();
+
+    cy.get("#filter-by-permission-select").select("Permission Allow");
+    cy.contains("Allow").should("exist");
+    cy.screenshot();
+  });
+
+  it("TC40016 User can filter permission Deny", () => {
+    login("boat", "19092545Boat");
+    cy.get("#devices-nav-link-sidebar").click();
+
+    cy.get("#filter-by-permission-select").select("Permission Deny");
+    cy.contains("Deny").should("exist");
+    cy.screenshot();
+  });
+  
 });
